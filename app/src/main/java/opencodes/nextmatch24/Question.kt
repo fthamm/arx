9141adae65a421216798c3a6ad39007b5aebb3a8
Jannik Hellenkamp <jannik.hellenkamp@rwth-aachen.de>
2018-03-24 20:09:12 +0100

    Added Question class

---
+++ app/src/main/java/opencodes/nextmatch24/Question.kt
@@ -0,0 +1,11 @@
+package opencodes.nextmatch24
+
+/**
+ * Created by Jannik on 24. Mär. 2018.
+ */
+class Question(pQuestion: String, pAnswer1: String, pAnswer2: String, pAnswer3: String) {
+    var question: String = pQuestion
+    var answer1: String = pAnswer1
+    var answer2: String = pAnswer2
+    var answer3: String = pAnswer3
+}
