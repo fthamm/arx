d01a304094b71570ac3c3cd7249c908f28bb9a53
Georg Ahnert <mail (at) geahnert.de>
2018-03-24 21:39:48 +0100

    Added Answers to Ask Tab, Fixed Colors

--- app/src/main/java/opencodes/nextmatch24/AskFragment.kt
+++ app/src/main/java/opencodes/nextmatch24/AskFragment.kt
@@ -11,6 +11,6 @@ class AskFragment : Fragment() {
     var questions: MutableList<Question> = mutableListOf()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.tab_fragment_match, container, false)
+        return inflater!!.inflate(R.layout.tab_fragment_ask, container, false)
     }
 }
